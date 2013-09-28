d46be5a6e09cf1c9ccfc9b30e54ced1a580e7796
fwsGonzo <fwsgonzo@hotmail.com>
2013-09-28 23:15:36 +0200

    Removed unnecessary files

--- Debug/test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cppcraft.exe Worlds\test
-
--- fbcraft_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo "----------Building project:[ fbcraft - Debug ]----------"
-	@$(MAKE) -f  "fbcraft.mk"
-clean:
-	@echo "----------Cleaning project:[ fbcraft - Debug ]----------"
-	@$(MAKE) -f  "fbcraft.mk" clean
