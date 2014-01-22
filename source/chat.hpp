0776eee06b6d180dea367f91b4642bca51ce6797
fwsGonzo <fwsgonzo@hotmail.com>
2014-01-22 09:39:47 +0100

    Early work on chat

--- source/chat.hpp
+++ source/chat.hpp
@@ -12,26 +12,30 @@
 
 namespace cppcraft
 {
-	
 	class Chatbox
 	{
+	public:
 		typedef unsigned int color_t;
-		
-		struct ChatboxLine
-		{
-			std::string text;
-			unsigned int color;
-			bool newline;
-		};
-		
-		std::vector<ChatboxLine> lines;
-		std::string typetext;
-		
-		int chatmode;
 		
 		void render();
 		void add(std::string text, color_t color, bool newline);
 		void addChat(std::string source, color_t scolor, std::string text, color_t tcolor);
+		
+	private:
+		struct ChatboxLine
+		{
+			std::string text;
+			color_t color;
+			bool newline;
+			
+			ChatboxLine() {}
+			ChatboxLine(std::string Text, color_t Color, bool NewLine) :
+				text(Text), color(Color), newline(NewLine) {}
+		};
+		
+		std::vector<ChatboxLine> lines;
+		std::string typetext;
+		bool typing;
 	};
 	extern Chatbox chatbox;
 }
