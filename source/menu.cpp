a47c42436595ec5d108a8b8fa76483dd0a88331f
fwsGonzo <fwsgonzo@hotmail.com>
2014-03-26 00:39:24 +0100

    Build-related inventory

--- source/menu.cpp
+++ source/menu.cpp
@@ -20,13 +20,17 @@ namespace cppcraft
 		inventory = Inventory(9, 5);
 		
 		// create default inventory
-		for (int x = 0; x < 4; x++)
-		{
-			inventory(x, quickbarY) = InventoryItem(IT_WOODPICK + x, ITT_ITEM, 1);
-		}
-		inventory(4, quickbarY) = InventoryItem(_WOODDOOR, ITT_BLOCK, 8);
-		inventory(5, quickbarY) = InventoryItem(_PLANK, ITT_BLOCK, 9999);
+		inventory(0, quickbarY) = InventoryItem(IT_DIAMPICK, ITT_ITEM, 1);
+		
+		inventory(1, quickbarY) = InventoryItem(_STONE, ITT_BLOCK, 9999);
+		inventory(2, quickbarY) = InventoryItem(_PLANK, ITT_BLOCK, 9999);
+		
+		inventory(3, quickbarY) = InventoryItem(_WOODSTAIR, ITT_BLOCK, 999);
+		inventory(4, quickbarY) = InventoryItem(_WOODPOLE, ITT_BLOCK, 999);
+		
+		inventory(5, quickbarY) = InventoryItem(_WOODDOOR, ITT_BLOCK, 8);
 		inventory(6, quickbarY) = InventoryItem(_LADDER, ITT_BLOCK, 999);
+		
 		inventory(7, quickbarY) = InventoryItem(_TORCH, ITT_BLOCK, 64);
 		inventory(8, quickbarY) = InventoryItem(_LANTERN, ITT_BLOCK, 64);
 		
