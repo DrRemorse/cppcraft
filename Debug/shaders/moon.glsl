8f665d711f8e9ff1a4a5f6fc7522aaeaffd855dd
fwsGonzo <fwsgonzo@hotmail.com>
2014-06-11 09:00:19 +0200

    Fixed older satelite shaders

--- Debug/shaders/moon.glsl
+++ Debug/shaders/moon.glsl
@@ -1,12 +1,8 @@
-#version 130
+#version 150
 #define VERTEX_PROGRAM
 #define FRAGMENT_PROGRAM
-#define POSTPROCESS
-
-precision mediump float;
 
 #ifdef VERTEX_PROGRAM
-
 uniform mat4 matproj;
 uniform mat4 matview;
 uniform mat4 matrot;
@@ -21,7 +17,6 @@ void main(void)
 	texCoord = in_vertex.xy + vec2(0.5);
 	
 	vec4 position = matview * vec4(in_vertex.xyz, 1.0);
-	
 	norm = normalize(position.xyz * mat3(matrot));
 	
 	gl_Position = matproj * position;
@@ -30,12 +25,11 @@ void main(void)
 #endif
 
 #ifdef FRAGMENT_PROGRAM
-
 uniform sampler2D texture;
-uniform float daylight;
 
-in vec2 texCoord;
-in vec3 norm;
+in  vec2 texCoord;
+in  vec3 norm;
+out vec4 color;
 
 void main(void)
 {
@@ -44,18 +38,11 @@ void main(void)
 	
 	//vec2 tex = pow(texCoord.st, vec2(1.0, 1.0 + horizon));
 	
-	vec4 color = texture2D(texture, texCoord.st);
-	
+	color = texture2D(texture, texCoord.st);
 	// make invisible below horizon
 	color.a *= pow(1.0 - horizon, 2.0);
 	
-#ifdef POSTPROCESS
-	// degamma
-	color.rgb = pow(color.rgb, vec3(1.0 / 2.2));
-#endif
 	//color.rgb += horizon * 0.5;
-	
-	gl_FragColor = color;
 }
 
 #endif
--- Debug/shaders/sun.glsl
+++ Debug/shaders/sun.glsl
@@ -1,7 +1,6 @@
-#version 130
+#version 150
 #define VERTEX_PROGRAM
 #define FRAGMENT_PROGRAM
-precision mediump float;
 
 #ifdef VERTEX_PROGRAM
 uniform mat4 matproj;
@@ -18,7 +17,6 @@ void main(void)
 	texCoord = in_vertex.xy + vec2(0.5);;
 	
 	vec4 position = matview * vec4(in_vertex.xyz, 1.0);
-	
 	norm = normalize(position.xyz * mat3(matrot));
 	
 	gl_Position = matproj * position;
@@ -29,24 +27,22 @@ void main(void)
 #ifdef FRAGMENT_PROGRAM
 uniform sampler2D texture;
 
-in vec2 texCoord;
-in vec3 norm;
+in vec2  texCoord;
+in vec3  norm;
+out vec4 color;
 
 void main(void)
 {
-	float horizon = clamp(-norm.y / 0.025,  0.0, 1.0);
+	color = texture2D(texture, texCoord);
 	
-	vec3 color = texture2D(texture, texCoord).rgb;
-	float alpha = smoothstep(0.0, 0.9, color.r);
+	const vec3 SUN_COLOR = vec3(1.0, 0.8, 0.6);
 	
-	color.rgb = mix(vec3(1.0, 0.8, 0.6), vec3(1.0), pow(alpha, 8.0));
+	color.a = smoothstep(0.0, 0.9, color.r);
+	color.rgb = mix(SUN_COLOR, vec3(1.0), pow(color.a, 8.0));
 	
 	// limit sun to horizon
-	alpha *= pow(1.0 - horizon, 3.0);
-	
-	//if (alpha < 0.1) discard;
-	
-	gl_FragColor = vec4(color, alpha);
+	float horizon = clamp(-norm.y / 0.025,  0.0, 1.0);
+	color.a *= pow(1.0 - horizon, 3.0);
 }
 
 #endif
