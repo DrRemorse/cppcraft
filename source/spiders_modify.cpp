bad4bf085df2785c0a9c50daa6bfa6e8f1e7ff09
Remorse <remorse@paddylee.com>
2015-03-19 22:54:16 -0400

    oops forgot to dereference

--- source/spiders_modify.cpp
+++ source/spiders_modify.cpp
@@ -244,7 +244,7 @@ namespace cppcraft
 
                 //memcpy(s->blockpt, sectorblock, sizeof(Sector::sectorblock_t));
 
-                s->blockpt = *sectorblock;
+                *s->blockpt = *sectorblock;
 
 		// flag sector as having modified blocks
 		s->contents = Sector::CONT_SAVEDATA;
